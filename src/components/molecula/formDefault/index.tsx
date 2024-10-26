10277da1120f2827239e189ccd39a3ee1d620ba7
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-26 13:39:26 -0300

    feat: criando o componente form dos meios de entrada do system

---
+++ src/components/molecula/formDefault/index.tsx
@@ -0,0 +1,57 @@
+import ButtonSubmit from "@/components/atomo/buttonSubmit"
+import CardReplace from "@/components/atomo/cardReplaceSignUp"
+import { ReactNode } from "react"
+
+type Props = {
+    variant: 'sinUp' | 'login'
+    children: ReactNode
+}
+
+export default function Form({ variant , children }: Props) {
+    return (
+        <div>
+            <h2 className="font-semibold text-3xl mb-10 text-slate-700">Capital Whise</h2>
+            <span className="font-bold text-2xl text-slate-700">
+                {variant == "login" ? ("Acesse sua conta") : ("Crie a sua conta") }
+            </span>
+
+            <p>
+                Conecte-se à nossa plataforma e tenha acesso às melhores oportunidades do mercado de ações.
+            </p>
+
+            <form action="" className="w-full mt-10 space-y-6">
+                
+                {children}
+
+                <div className=" flex space-x-2">
+                    <input type="checkbox" />
+                    <p className="font-semibold text-slate-600">
+                        Lembre-me
+                    </p>
+                </div>
+                    
+                <ButtonSubmit
+                    content={variant == "login" ? ("Entrar") : ("Criar")}
+                />
+
+                <div className="w-full h-[1px] bg-slate-800 my-10"></div>
+
+                <CardReplace
+                    title={variant == "login" ? ("Não tem uma conta") : ("Já tem uma conta?")}
+                    subtitle={variant == "login" ? ("Se inscreva gratuitamente") : ("Entre agora mesmo")}
+                    href={variant == "login" ? ("/signUp") : ("/login")}
+                    color="#6cd55e"
+                />
+
+                {variant == "login" &&
+                    <CardReplace
+                        title="Esqueceu a sua senha?"
+                        subtitle=""
+                        href="/"
+                    />
+                }
+
+            </form>
+        </div>
+    )
+}
