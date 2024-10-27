cebb282f152b0bb427ebd4c38f91edd254ce837a
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-27 11:36:19 -0300

    feat: criando os botóes que serão usados no desyng system do componente da carteira

---
+++ src/components/atomo/buttonWallet/index.tsx
@@ -0,0 +1,36 @@
+import { MoveUpRight } from "lucide-react"
+import { MoveDownLeft } from "lucide-react"
+import { ArrowRightLeft } from "lucide-react"
+import { ComponentProps, ReactNode } from "react"
+import { twJoin } from "tailwind-merge"
+
+type Props = ComponentProps<'button'> & {
+    children?: ReactNode
+    variant: "saque" | "deposito" | "transferencia" | "default"
+}
+
+export default function ButtonWallet({ variant, children, ...props }: Props) {
+    return (
+        <button {...props} title={variant} className={twJoin(
+            "w-[55px] h-[55px] flex items-center justify-center rounded-full",
+            variant == "deposito" && "bg-[#6FD660] text-slate-50 hover:bg-[#336D3D]",
+            variant == "saque" && "bg-slate-900 text-slate-50 hover:bg-slate-800",
+            variant == "transferencia" && "bg-slate-300 hover:bg-slate-400"
+        )}>
+
+            {variant == "deposito" &&
+                <MoveDownLeft />
+            }
+            {variant == "saque" &&
+                <MoveUpRight />
+            }
+            {variant == "transferencia" &&
+                <ArrowRightLeft />
+            }
+
+            {children}
+
+        </button>
+    )
+}
+    
