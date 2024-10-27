e750c23b3786372939904f70e5e4a00ba8e3905a
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-27 14:51:58 -0300

    feat: criando o item que ira mostrar o histórico de transações

--- src/components/atomo/buttonWallet/index.tsx
+++ src/components/atomo/buttonWallet/index.tsx
@@ -7,15 +7,17 @@ import { twJoin } from "tailwind-merge"
 type Props = ComponentProps<'button'> & {
     children?: ReactNode
     variant: "saque" | "deposito" | "transferencia" | "default"
+    classVariant?: string
 }
 
-export default function ButtonWallet({ variant, children, ...props }: Props) {
+export default function ButtonWallet({ classVariant , variant, children, ...props }: Props) {
     return (
         <button {...props} title={variant} className={twJoin(
             "w-[55px] h-[55px] flex items-center justify-center rounded-full",
             variant == "deposito" && "bg-[#6FD660] text-slate-50 hover:bg-[#336D3D]",
             variant == "saque" && "bg-slate-900 text-slate-50 hover:bg-slate-800",
-            variant == "transferencia" && "bg-slate-300 hover:bg-slate-400"
+            variant == "transferencia" && "bg-slate-300 hover:bg-slate-400",
+            classVariant
         )}>
 
             {variant == "deposito" &&
