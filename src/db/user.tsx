5562aa1ea61bc01dc04294dc2a373d8d7514d71d
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-27 08:28:28 -0300

    feat: criando a lógica da criação das contas

--- src/db/user.tsx
+++ src/db/user.tsx
@@ -1,47 +1,26 @@
 export const usersDb = [
         {
-            "id": 1,
+            "id": "1",
             "nome": "João Silva",
             "email": "joao.silva@example.com",
-            "cpf": "123.456.789-00",
-            "endereco": {
-                "cep": "12345-678",
-                "logradouro": "Rua das Flores",
-                "numero": "123",
-                "bairro": "Centro",
-                "cidade": "São Paulo",
-                "estado": "SP"
-            },
-            "senha": "Senha@123"
+            "cep": "12345-678",
+            "senha": "Senha@123",
+            "saldo":0.0
         },
         {
-            "id": 2,
+            "id": "2",
             "nome": "Maria Souza",
             "email": "maria.souza@example.com",
-            "cpf": "987.654.321-00",
-            "endereco": {
-                "cep": "87654-321",
-                "logradouro": "Avenida Paulista",
-                "numero": "456",
-                "bairro": "Bela Vista",
-                "cidade": "São Paulo",
-                "estado": "SP"
-            },
-            "senha": "Maria@2024"
+            "cep": "87654-321",
+            "senha": "Maria@2024",
+            "saldo":0.0
         },
         {
-            "id": 3,
+            "id": "3",
             "nome": "Carlos Pereira",
             "email": "carlos.pereira@example.com",
-            "cpf": "111.222.333-44",
-            "endereco": {
-                "cep": "11223-445",
-                "logradouro": "Rua do Sol",
-                "numero": "789",
-                "bairro": "Jardim",
-                "cidade": "Rio de Janeiro",
-                "estado": "RJ"
-            },
-            "senha": "Carlos#321"
+            "cep": "11223-445",
+            "senha": "Carlos#321",
+            "saldo":0.0
         }
     ]
