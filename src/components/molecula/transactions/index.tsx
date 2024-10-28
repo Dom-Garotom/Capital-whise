6dc06c11b26e8eb824d0e99aaeb994e15126de65
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-28 06:49:28 -0300

    refactor: ajustando o tamanho do componente

--- src/components/molecula/transactions/index.tsx
+++ src/components/molecula/transactions/index.tsx
@@ -5,7 +5,7 @@ export default function Transactions() {
 
 
     return (
-        <div className="flex flex-col justify-center max-w-[400px] max-h-[350px]  w-full space-y-4  px-8  py-5 rounded-[10px] bg-slate-100 shadow-2xl">
+        <div className="flex flex-col justify-center max-w-[400px] max-h-[500px] h-full  w-full space-y-4  px-8  py-5 rounded-[10px] bg-slate-100 shadow-2xl">
             <div className='font-semibold text-lg'>Transações recentes</div>
 
             <div className="overflow-auto overflow scrollbar-hidden py-5 space-y-3">
